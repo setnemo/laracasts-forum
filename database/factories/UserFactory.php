46ac882916bbbdf75708feb92ac329646dee4cec
Artem <setnemo@gmail.com>
2019-07-09 09:15:08 +0300

    episode 1 done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,3 +26,24 @@ $factory->define(User::class, function (Faker $faker) {
     ];
 });
 
+$factory->define(App\Thread::class, function(Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(App\Reply::class, function(Faker $faker) {
+    return [
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'body' => $faker->paragraph
+    ];
+});
--- history.md
+++ history.md
@@ -5,4 +5,9 @@
 php artisan make:model Thread -mr
 php artisan migrate
 php artisan make:model Reply -mc
+php artisan tinker
+>>> factory('App\Thread', 50)->create();
+php artisan migrate:refresh 
+>>> $threads = factory('App\Thread', 50)->create();
+>>> $threads->each(function ($thread) { factory('App\Reply', 10)->create(['thread_id' => $thread->id]); });
 ```
